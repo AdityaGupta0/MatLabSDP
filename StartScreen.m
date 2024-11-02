d745cc201973a35d242505faf9809ac5b3ac2d19
AdityaGupta0 <48954567+AdityaGupta0@users.noreply.github.com>
2024-11-02 19:52:47 -0400

    Welcome & level select screens
    Added welcome screen with game name and a play button which the user can click to enter the level select screen.

--- StartScreen.m
+++ StartScreen.m
@@ -1,3 +1,19 @@
-startSceen = simpleGameEngine('sprite_sheet.png',511,511,1,[0,0,245]);
+clear
+
+startScreen = simpleGameEngine('sprite_sheet.png',512,512,1,[0,0,245]);
 startScreenArray = [1,2,1;1,3,1;1,1,1];
-drawScene(startSceen,startScreenArray)
+drawScene(startScreen,startScreenArray)
+
+
+%Determine which sprite was clicked
+% r = row, c= column, b = button (1 = left, 2 = middle, 3 = right)
+[r,c,b] = getMouseInput(startScreen);
+
+%If the left mouse button is clicked, flip over the card that was
+%clicked
+
+levelSelectScene = simpleGameEngine('Level_select_sprite_sheet.png',512,512,1);
+levelselectArray = [1,5,6,1,1;1,7,8,9,1;1,10,11,12,1;1,2,3,4,1;1,1,1,1,1];
+if b == 1 && (512<c<1024) && (512<r<1024)
+    drawScene(levelSelectScene,levelselectArray)
+end
