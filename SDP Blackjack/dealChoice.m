84977c5cf54de1fb6ca2fedadf89d1ea98c44286
AndrewK1300 <kendall.249@buckeyemail.osu.edu>
2024-11-20 17:37:19 -0500

    Update dealChoice.m

--- SDP Blackjack/dealChoice.m
+++ SDP Blackjack/dealChoice.m
@@ -1,6 +1,19 @@
 function [choice] = dealChoice(gamble)
     global total;
-    
+    % tells the opponent to hit or stand
+    while true
+        if gamble > 0 && gamble < 100
+        elseif gamble >= 100 && gamble < 300
+        elseif gamble >= 300 && gamble < 800
+        elseif gamble >= 800 && gamble < 2000
+        elseif gamble >= 2000 && gamble < 5000
+        elseif gamble >= 5000 && gamble < 10000
+        elseif gamble >= 10000 && gamble < 30000
+        elseif gamble >= 30000
+        end
+    end
+end
+    %{
     while true
         if total(2) <= total(1) && ~(total(2)>18 && total(2)==total(1))
             drawCard(1,2)
@@ -10,3 +23,4 @@ function [choice] = dealChoice(gamble)
         end
     end
 end
+    %}
