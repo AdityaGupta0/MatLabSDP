f6a39c2653a0321c5d04aa47981add602603a2cc
Reece <reece.lagowskiii@gmail.com>
2024-11-20 16:55:27 -0500

    forgot code lol

--- SDP Blackjack/dealChoice.m
+++ SDP Blackjack/dealChoice.m
@@ -1,4 +1,12 @@
 function [choice] = dealChoice(gamble)
-global total;
-
+    global total;
+    
+    while true
+        if total(2) <= total(1) && ~(total(2)>18 && total(2)==total(1))
+            drawCard(1,2)
+            graphicOutput(2)
+        else
+            break
+        end
+    end
 end
